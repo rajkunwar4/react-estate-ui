ea02d0ce5800f1dacb7014ec7c3b76d931a06f90
rajkunwar4 <40rajkunwar@gmail.com>
2024-06-21 15:45:46 +0530

    client routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
-import Navbar from "./components/navbar/Navbar";
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import Navbar from "./components/navbar/navbar/Navbar";
 import HomePage from "./routes/homePage/HomePage";
-import "./layout.scss";
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import ListPage from './routes/listPage/ListPage';
+import Layout from './routes/layout/layout';
+import SinglePage from './routes/singlePage/SinglePage';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path: "/list",
+          element: <ListPage/>,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage/>,
+        },
+      ]
+    },
+    
+  ]);
+
+ 
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        {" "}
-        <HomePage />
-      </div>
-    </div>
+    
+    
+    <RouterProvider router={router}/>
   );
 }
 
